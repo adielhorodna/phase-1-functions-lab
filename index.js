f7cf777b2af04935b0a815e51b88aa4b0b093b20
Adiel Eichenstein <adiel.eich@gmail.com>
2022-12-13 19:35:30 -0500

    commit tree 1039c91632b74385cea78da824e524cdf93e64f0

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+
+      function distanceFromHqInBlocks(location){
+        if (location < 42) {
+console.log(location)
+        return 42 - location 
+      }
+    
+    else if (location > 42) {
+return location -42
+    }
+      }
+      
+
+
+      
